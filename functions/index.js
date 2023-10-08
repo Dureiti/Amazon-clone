77ab9f9749eb46593a7a76dcedee2daa7e9997fa
Dureiti <140558567+Dureiti@users.noreply.github.com>
2023-10-08 15:15:07 +0300

    Amazon

---
+++ functions/index.js
@@ -0,0 +1,42 @@
+const functions = require("firebase-functions");
+const express = require("express");
+const cors = require("cors");
+const stripe = require("stripe")(
+	"sk_test_51Nw1WmEYyMxP0XsgaYKDDevzst6u94sSEhnNsrboKxv83g6aolvYksKMWn1Dn3XbdldVRS8gZBVxv9ufuzSU1Ixk00orO5a78U"
+);
+// App config
+const app = express();
+// Middlewares
+app.use(cors({ origin: true }));
+app.use(express.json());
+
+app.get("/", (request, response) => response.status(200).send("hello world"));
+
+app.post("/payments/create", async (request, response) => {
+	const total = request.query.total;
+
+	console.log("Payment Request Received for this amount >>> ", total);
+	try {
+		const paymentIntent = await stripe.paymentIntents.create({
+			amount: parseInt(total), //subunits of the currency
+			currency: "usd",
+		});
+
+		// Ok -created
+		response.status(201).send({
+			clientSecret: paymentIntent.client_secret,
+		});
+	} catch (error) {
+		console.log(error.message);
+		response.status(500).send("something went wrong!");
+	}
+});
+app.listen(5500, (err) => {
+	if (!err) {
+		console.log(`listening on port 5500`);
+	}
+});
+// Listen command
+// exports.api = functions.https.onRequest(app);
+// I stop on 1.18 video  there is a browser error I have to solve the error //
+// this one is api key(baseurl)  http://127.0.0.1:5001/project-207b1/us-central1/api
--- src/App.js
+++ src/App.js
@@ -1,25 +1,86 @@
-import logo from './logo.svg';
-import './App.css';
+import Checkout from "./Checkout";
+import Header from "./Header";
+import Home from "./Home";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Login from "./Login";
+import { useStateValue } from "./StateProvider";
+import React, { useEffect } from "react";
+import { auth } from "./Firebase";
+import Payment from "./Payment";
+import { loadStripe } from "@stripe/stripe-js";
+import { Elements } from "@stripe/react-stripe-js";
+import Orders from "./Orders";
+
+const promise = loadStripe(
+	"pk_test_51Nw1WmEYyMxP0XsgJRnLouJOLW5DZQ2sy4UwDHDKVwIST8xlcvUeMWOPXvZxPJiHrDg6bI5btVLXCJL28XsVgNNu003C67SQEg"
+);
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [{}, dispatch] = useStateValue();
+
+	useEffect(() => {
+		auth.onAuthStateChanged((authUser) => {
+			if (authUser) {
+				dispatch({
+					type: "SET_USER",
+					user: authUser,
+				});
+			} else {
+				dispatch({
+					type: "SET_USER",
+					user: null,
+				});
+			}
+		});
+	}, []);
+
+	return (
+		<Router>
+			<div className="App">
+				<Routes>
+					<Route path="/login" element={<Login />} />
+					<Route
+						path="/Payment"
+						element={
+							<>
+								<Header />
+								<Elements stripe={promise}>
+									<Payment />
+								</Elements>
+							</>
+						}
+					/>
+					<Route
+						path="/"
+						element={
+							<>
+								<Header />
+								<Home />
+							</>
+						}
+					/>
+					<Route
+						path="/checkout"
+						element={
+							<>
+								<Header />
+								<Checkout />
+							</>
+						}
+					/>
+					<Route
+						path="/Orders"
+						element={
+							<>
+								<Header />
+								<Orders />
+							</>
+						}
+					/>
+				</Routes>
+			</div>
+		</Router>
+	);
 }
 
 export default App;
