a62e0e00553949d3840a80d5db83207438283452
Dureiti <140558567+Dureiti@users.noreply.github.com>
2023-10-10 20:45:55 +0300

    error fixed

--- functions/index.js
+++ functions/index.js
@@ -1,4 +1,4 @@
-const functions = require("firebase-functions");
+require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const stripe = require("stripe")(process.env.SECRET_KEY);
